df23ced4838cb99e91674b338767470728d1f239
filipao223 <jpmontenegro67@gmail.com>
2020-03-15 15:20:26 +0000

    Changed test and build scripts to include AST tree testing.

--- meta2/build.sh
+++ meta2/build.sh
@@ -1,6 +1,7 @@
 # Start building commands
 LEX="lex jucompiler.l"
-CLANG="clang-3.9 -Wall lex.yy.c -o jucompiler"
+YACC="yacc -d jucompiler.y"
+CLANG="clang-3.9 -Wall -Wno-unused-function -o jucompiler lex.yy.c y.tab.c -lm"
 
 # Check for any arguments
 while getopts "v" OPTION; do
@@ -8,26 +9,28 @@ while getopts "v" OPTION; do
     v)
         CLANG=$CLANG" -v"
         LEX="lex -v jucompiler.l"
+        YACC="yacc -d --debug --verbose jucompiler.y"
     esac
 done
 
 # Compile lex file, checking for build success
 if $LEX; then
 
-    # Compile using clang, checking for build success
-    if $CLANG; then
+    # Compile yacc file, checking for build success
+    if $YACC; then
 
-        # Zip the lex file if build successful
-        zip jucompiler.zip jucompiler.l
+        # Compile using clang, checking for build success
+        if $CLANG; then
+            # Zip the lex file if build successful
+            zip jucompiler.zip jucompiler.l jucompiler.y
+        else
+            exit
+        fi
 
     else
-
         exit
-
     fi
 
 else
-
     exit
-
 fi
--- meta2/test.sh
+++ meta2/test.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
 # Clean previous tests
-rm test/failed/*.out
+rm test/lex/failed/*.out
+rm test/yacc/failed/*.out
 
 # Build jucompiler if not already built
 if ! [ -f jucompiler ]; then
@@ -21,48 +23,83 @@ SKIPSTR="${BLUE}SKIP"
 
 shopt -s nullglob
 
+padlimit=50
+pad=$(printf '%*s' "$padlimit")
+pad=${pad// /-}
+padlength=50
+
+test(){
+    # Test every .juc or .java file
+    for filename in ./test/$1/input/*.{juc,java}; do
+        FILE=${filename##*/}
+
+        # Run jucompiler with this test case
+        if [ "$1" == "lex" ]; then
+            if [ $FILE == "errors.java" ] || [ $FILE == "InsertionSort.java" ]; then
+                ./jucompiler -e1 < $filename > output.txt
+            else
+                ./jucompiler -l < $filename > output.txt
+            fi
+        else
+            ./jucompiler -t < $filename > output.txt
+        fi
+
+        # Check if an .out file doesn't exist for this test
+        if ! [ -f ./test/$1/output/${FILE%.*}.out ]; then
+            printf '%s%*.*s%s\n' "$FILE" 0 $(($padlength - ${#FILE})) "$pad" "$SKIPSTR"
+            ((SKIPPED+=1))
+            tput sgr0
+            continue
+        fi
+        
+        # Otherwise run a diff on them
+        if [ "$(diff -y --suppress-common-lines ./test/$1/output/${FILE%.*}.out output.txt | wc -l)" -ne "0" ]; then
+            # If files are different, move it to failed directory (creating it if it non-existent)
+            mkdir -p test/$1/failed && cp output.txt test/$1/failed/${FILE%.*}.out
+            printf '%s%*.*s%s\n' "$FILE" 0 $((padlength - ${#FILE} )) "$pad" "$FAILSTR"
+            ((FAILED+=1))
+            tput sgr0
+        else
+            printf '%s%*.*s%s\n' "$FILE" 0 $((padlength - ${#FILE} )) "$pad" "$PASSSTR"
+            ((PASSED+=1))
+            tput sgr0
+        fi
+    done
+}
+
+######################################
+## Regression tests (test Lex only) ##
+######################################
+
 echo ""
-echo "Starting test..."
+echo "Starting lex test..."
 echo ""
 
+test lex
 
-# Test every .juc or .java file
-for filename in ./test/input/*.{juc,java}; do
-    FILE=${filename##*/}
+echo ""
+echo ""
 
-    padlimit=50
-    pad=$(printf '%*s' "$padlimit")
-    pad=${pad// /-}
-    padlength=50
+# Print final stats
+echo "PASSED: $PASSED"
+echo "FAILED: $FAILED"
+echo "SKIPPED: $SKIPPED"
 
-    # Run jucompiler with this test case
-    if [ $FILE == "errors.java" ] || [ $FILE == "InsertionSort.java" ]; then
-        ./jucompiler -e1 < $filename > output.txt
-    else
-        ./jucompiler -l < $filename > output.txt
-    fi
+PASSED=0
+FAILED=0
+SKIPPED=0
 
-    # Check if an .out file doesn't exist for this test
-    if ! [ -f ./test/output/${FILE%.*}.out ]; then
-	printf '%s%*.*s%s\n' "$FILE" 0 $(($padlength - ${#FILE})) "$pad" "$SKIPSTR"
-        ((SKIPPED+=1))
-	tput sgr0
-	continue
-    fi
-	
-    # Otherwise run a diff on them
-    if [ "$(diff -y --suppress-common-lines ./test/output/${FILE%.*}.out output.txt | wc -l)" -ne "0" ]; then
-        # If files are different, move it to failed directory (creating it if it non-existent)
-        mkdir -p test/failed && cp output.txt test/failed/${FILE%.*}.out
-	printf '%s%*.*s%s\n' "$FILE" 0 $((padlength - ${#FILE} )) "$pad" "$FAILSTR"
-        ((FAILED+=1))
-	tput sgr0
-    else
-	printf '%s%*.*s%s\n' "$FILE" 0 $((padlength - ${#FILE} )) "$pad" "$PASSSTR"
-        ((PASSED+=1))
-	tput sgr0
-    fi
-done
+
+######################################
+##      Test yacc (AST tree)        ##
+######################################
+
+echo ""
+echo ""
+echo "Starting yacc test..."
+echo ""
+
+test yacc
 
 echo ""
 echo ""
